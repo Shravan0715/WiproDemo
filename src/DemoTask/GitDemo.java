f3248202328728339b7b846a096ea92d57f136c6
Shravan0715 <shravangalagali573@gmail.com>
2025-01-31 12:38:54 +0530

    Initial commit

---
+++ src/DemoTask/GitDemo.java
@@ -0,0 +1,11 @@
+package DemoTask;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello this is my Demo project");
+		
+	}
+
+}
