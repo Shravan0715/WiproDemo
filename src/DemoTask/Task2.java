948adb7e82df3b763c5721024173f7ecf9758b0c
Shravan0715 <shravangalagali573@gmail.com>
2025-01-31 13:05:55 +0530

    changes added

--- src/DemoTask/Task2.java
+++ src/DemoTask/Task2.java
@@ -5,6 +5,12 @@ public class Task2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Updated version of Demo project");
+		System.out.println("even numbers from 0-10 are:");
+		for(int i=0; i<=10; i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
 	}
 
 }
