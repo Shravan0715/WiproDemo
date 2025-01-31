c33faad311ab19d6ae491b5d7da88d462730bde1
Shravan0715 <shravangalagali573@gmail.com>
2025-01-31 12:51:25 +0530

    updated changes

---
+++ src/DemoTask/Task2.java
@@ -0,0 +1,10 @@
+package DemoTask;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Updated version of Demo project");
+	}
+
+}
